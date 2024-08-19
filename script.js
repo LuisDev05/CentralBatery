44d93608f1568d3f0aa0626cd747aa5049b87094
LuisDev05 <luisfernandogoncalvesmendes5@gmail.com>
2024-08-19 11:25:30 -0400

    Batery 1.1

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+
 const carousel = document.querySelector('.carousel');
 const carouselInner = carousel.querySelector('.carousel-inner');
 const carouselItems = carouselInner.querySelectorAll('.carousel-item');
